40de320231a5be3b601d5ac95e7ada2ca6d52584
Murilo Wolfart <mwolfart@mwolfart.local>
2021-01-13 13:43:24 -0300

    Add modal and button components; Add FontAwesomeIcons

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Modal } from './components/modal/modal.component';
+import { Button } from './components/button/button.component';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Modal>
+        Testing
+      </Modal>
     </div>
   )
 }
