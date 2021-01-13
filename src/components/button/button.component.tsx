c447b2d9881039dea659583b3fb63dec7a404f84
Murilo Wolfart <mwolfart@mwolfart.local>
2021-01-13 13:43:24 -0300

    Add modal and button components; Add FontAwesomeIcons

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    background-image: linear-gradient(45deg, #BF0A45, #E4305D 66%, #EE7321);
+    border-radius: 12px;
+    border-width: 0px;
+    padding: 0 36px 0 36px;
+    color: #EEEEEE;
+    text-align: center;
+    font-family: Montserrat, Arial, sans-serif;
+    font-size: 16px;
+    font-weight: bold;
+    line-height: 48px;
+    letter-spacing: 1.5px;
+`
