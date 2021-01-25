9fd2bba8ac4bb015f9c53f9a701ab5259af1671a
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-25 18:43:34 -0300

    [login]: apply requested changes

---
+++ src/components/ui/canvasRectangle.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CanvasRectangle = styled.div`
+    position: absolute;
+    min-height: 100%;
+    min-width: 100vw;
+    display: flex;
+    justify-content: center;
+    background-color: #AAAAAA55;
+    place-items: center;
+`
--- src/scenes/login/login.tsx
+++ src/scenes/login/login.tsx
@@ -20,3 +20,4 @@ export const LoginScene: VFC = () => {
     </Canvas>
   )
 }
+
