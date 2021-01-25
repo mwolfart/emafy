1c3512d29fe8c222640888dcad03712a24ca6191
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-25 15:27:47 -0300

    [login]: refactored close-button, applied requested fixes to ui elements

---
+++ src/components/closeButton/closeButton.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { VFC } from "react"
+import styled from "styled-components"
+import { CleanButton } from "../ui"
+
+const CloseButtonIcon = styled.i`
+    text-align: right;
+`
+
+type Props = {
+    onClickCallback: () => void
+}
+
+export const CloseButton: VFC<Props> = ({ onClickCallback }) => (
+    <CleanButton onClick={ onClickCallback }>
+        <CloseButtonIcon className="fa fa-times fa-2x close-button" />
+    </CleanButton>
+)
