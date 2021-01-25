13f291fcc5f6cdc5c54b446ab7ef67a8f7163b13
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-25 18:43:34 -0300

    [login]: apply requested changes

--- src/components/canvas/canvas.tsx
+++ src/components/canvas/canvas.tsx
@@ -1,15 +1,5 @@
 import React, { FC } from 'react'
-import styled from 'styled-components'
-
-const CanvasRectangle = styled.div`
-    position: absolute;
-    min-height: 100%;
-    min-width: 100vw;
-    display: flex;
-    justify-content: center;
-    background-color: #AAAAAA55;
-    place-items: center;
-`
+import { CanvasRectangle } from '../ui/canvasRectangle'
 
 export const Canvas: FC = ({ children }) =>
     <CanvasRectangle>
--- src/scenes/login/login.tsx
+++ src/scenes/login/login.tsx
@@ -1,16 +1,20 @@
 import React, { VFC } from 'react';
 import { Modal } from '../../components/modal/modal';
 import { Canvas } from '../../components/canvas/canvas';
-import { Button } from '../../components/ui/index';
+import { Button } from '../../components/ui/';
 
 export const LoginScene: VFC = () => {
     const login = () => {
 
     }
 
+    const closeLoginModal = () => {
+
+    }
+
     return (
         <Canvas>
-        <Modal closeModalCallback={ () => {} }>
+        <Modal closeModalCallback={ closeLoginModal }>
             <div className="title">Sign in</div>
             <p>To start using the platform, please login using the Spotify API through the link below</p>
             <Button onClick={login}>Login using Spotify</Button>
