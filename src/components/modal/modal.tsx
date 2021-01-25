3101aa6fac5330211baf1fa772ee6ec524f25ae5
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-25 15:56:52 -0300

    [login]: Refactored buttons and UI elements

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,49 +1,16 @@
 import React, { FC } from 'react'
-import styled from 'styled-components'
 import 'font-awesome/css/font-awesome.min.css'
 import { CloseButton } from '../closeButton/closeButton'
+import { ModalRectangle, Props as ModalRectProps } from '../ui/modalRectangle'
 
-type ModalProperties = {
-    closeModalCallback?: () => void
-    large?: boolean
-}
+type Props = {
+    closeModalCallback: () => void
+} & ModalRectProps
 
-const ModalRectangle = styled.div`
-    height: ${ (props: ModalProperties) => props.large ? "500px" : "250px" };
-    width: ${ (props: ModalProperties) => props.large ? "800px" : "400px" };
-    display: flex;
-    flex-direction: column;
-    background-color: #F2F2F2;
-    border-radius: 16px;
-    border-width: 0px;
-    box-shadow: 0px 80px 100px;
-    padding-top: 32px;
-    padding-left: 32px;
-    padding-right: 32px;
-    padding-bottom: 48px;
-    color: #757575;
-    text-align: center;
-    font-family: Montserrat, "Trebuchet MS", Arial, sans-serif;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 28px;
-
-    p {
-        padding-bottom: 12px;
-    }
-
-    & .title {
-        font-size: 24px;
-        color: #222222;
-        line-height: 40px;
-        font-weight: 600;
-    }
-`
-
-export const Modal: FC<ModalProperties> = ({ children, closeModalCallback, large }) => {
+export const Modal: FC<Props> = ({ children, closeModalCallback, large }) => {
     return (
         <ModalRectangle large={large}>
-            <CloseButton onClickCallback={closeModalCallback || (() => 0)}/>
+            <CloseButton onClickCallback={closeModalCallback}/>
             { children }
         </ModalRectangle>
     )
---
+++ src/components/ui/modalRectangle.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export type Props = {
+    large?: boolean
+}
+
+export const ModalRectangle = styled.div`
+    height: ${ ({large}: Props) => large ? "500px" : "250px" };
+    width: ${ ({large}: Props) => large ? "800px" : "400px" };
+    display: flex;
+    flex-direction: column;
+    background-color: #F2F2F2;
+    border-radius: 16px;
+    border-width: 0px;
+    box-shadow: 0px 80px 100px;
+    padding-top: 32px;
+    padding-left: 32px;
+    padding-right: 32px;
+    padding-bottom: 48px;
+    color: #757575;
+    text-align: center;
+    font-family: Montserrat, "Trebuchet MS", Arial, sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 28px;
+
+    p {
+        padding-bottom: 12px;
+    }
+
+    & .title {
+        font-size: 24px;
+        color: #222222;
+        line-height: 40px;
+        font-weight: 600;
+    }
+`
