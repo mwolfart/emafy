51332ffc7528e972f00a25639a404318c64635a7
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-26 20:18:05 -0300

    [login] Run prettier, eslint; remove canvasRectangle

--- src/components/closeButton/closeButton.tsx
+++ src/components/closeButton/closeButton.tsx
@@ -1,18 +1,18 @@
-import React, { VFC} from "react"
-import styled from "styled-components"
+import React, { VFC } from 'react'
+import styled from 'styled-components'
 import 'font-awesome/css/font-awesome.min.css'
-import { CleanButton } from "../ui/"
+import { CleanButton } from '../ui/'
 
 const CloseButtonIcon = styled.i`
-    text-align: right;
+  text-align: right;
 `
 
 type Props = {
-    onClickCallback: () => void
+  onClickCallback: () => void
 }
 
 export const CloseButton: VFC<Props> = ({ onClickCallback }) => (
-    <CleanButton onClick={ onClickCallback } aria-label="Close modal">
-        <CloseButtonIcon className="fa fa-times fa-2x close-button" />
-    </CleanButton>
+  <CleanButton onClick={onClickCallback} aria-label="Close modal">
+    <CloseButtonIcon className="fa fa-times fa-2x close-button" />
+  </CleanButton>
 )
