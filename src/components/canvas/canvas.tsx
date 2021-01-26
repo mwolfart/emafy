9df9bace6acf1781739cd8595321307768eb0f44
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-26 20:18:05 -0300

    [login] Run prettier, eslint; remove canvasRectangle

--- src/components/canvas/canvas.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react'
-import { CanvasRectangle } from '../ui/canvasRectangle'
-
-export const Canvas: FC = ({ children }) =>
-    <CanvasRectangle>
-        { children }
-    </CanvasRectangle>
-    
