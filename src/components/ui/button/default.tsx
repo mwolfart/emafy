3e82b55b03443ad0b98258d1717c2be739e350f1
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-26 20:18:05 -0300

    [login] Run prettier, eslint; remove canvasRectangle

--- src/components/ui/button/default.tsx
+++ src/components/ui/button/default.tsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
-    background-image: linear-gradient(45deg, #BF0A45, #E4305D 66%, #EE7321);
-    border-radius: 12px;
-    border-width: 0px;
-    padding: 0px 36px;
-    color: #EEEEEE;
-    text-align: center;
-    font-family: Montserrat, "Trebuchet MS", Arial, sans-serif;
-    font-size: 16px;
-    font-weight: bold;
-    line-height: 48px;
-    letter-spacing: 1.5px;
-    width: fit-content;
-    align-self: center;
+  background-image: linear-gradient(45deg, #bf0a45, #e4305d 66%, #ee7321);
+  border-radius: 12px;
+  border-width: 0px;
+  padding: 0px 36px;
+  color: #eeeeee;
+  text-align: center;
+  font-family: Montserrat, 'Trebuchet MS', Arial, sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 48px;
+  letter-spacing: 1.5px;
+  width: fit-content;
+  align-self: center;
 `
