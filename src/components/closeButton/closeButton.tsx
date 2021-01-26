7affe82f2c889abf108b59c69ab4ee6d6d6e0bdb
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-26 13:47:38 -0300

    [ui-components]: move font-awesome import to correct file

--- src/components/closeButton/closeButton.tsx
+++ src/components/closeButton/closeButton.tsx
@@ -1,5 +1,6 @@
 import React, { VFC} from "react"
 import styled from "styled-components"
+import 'font-awesome/css/font-awesome.min.css'
 import { CleanButton } from "../ui/"
 
 const CloseButtonIcon = styled.i`
