e6b6154ed9bb2c1126a2498daf0a677498ceae91
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-27 15:54:15 -0300

    [login] Use authenticate and login methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { login } from 'api/credentials'
 import React from 'react'
 import './App.css'
 import { LoginScene } from './scenes/login/login'
 
-const App: () => JSX.Element = () => <LoginScene />
+const App: () => JSX.Element = () => {
+  const loggedIn = login(() => {})
+  return loggedIn ? <div>adasd</div> : <LoginScene />
+}
 
 export default App
