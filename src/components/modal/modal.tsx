d7b601d082734248a0cd0c075a03bf82342f536f
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-26 23:17:23 -0300

    [ui-components] Simplified imports

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from 'react'
 import { CloseButton } from '../closeButton/closeButton'
-import { ModalRectangle, Props as ModalRectProps } from '../ui/modalRectangle'
+import {
+  ModalRectangle,
+  Props as ModalRectProps,
+} from 'components/ui/modalRectangle'
 
 type Props = {
   closeModalCallback: () => void
 } & ModalRectProps
 
-export const Modal: FC<Props> = ({ children, closeModalCallback, large }) =>
+export const Modal: FC<Props> = ({ children, closeModalCallback, large }) => (
   <ModalRectangle large={large}>
     <CloseButton onClickCallback={closeModalCallback} />
     {children}
   </ModalRectangle>
+)
