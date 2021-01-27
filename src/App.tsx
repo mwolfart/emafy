1e4dd4430a2272730951dde9f73001bec62292ff
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-01-26 22:53:20 -0300

    [ui-components] Remove unnecessary returns

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import './App.css'
 import { LoginScene } from './scenes/login/login'
 
-const App: () => JSX.Element = () => {
-  return <LoginScene />
-}
+const App: () => JSX.Element = () => <LoginScene />
 
 export default App
