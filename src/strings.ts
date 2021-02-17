a9d2bc75d429e813c53cd678a41973efd90e5dcb
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-17 17:14:22 -0300

    [i18n]: add strings file

---
+++ src/strings.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+
+export const strings = {
+  components: {
+    closeButton: 'Close Button',
+  },
+  scenes: {
+    login: {
+      signin: 'Sign in',
+      instructions: 'To start using the platform, please login using the Spotify API through the link below',
+      loginUsingSpotify: 'Login using Spotify',
+    },
+  },
+  api: {
+    credentials: {
+      errorAuth: 'There was an error during the authentication, please, refresh the webpage and try again',
+      errorLoginFailed: 'Login failed',
+    },
+  },
+}
