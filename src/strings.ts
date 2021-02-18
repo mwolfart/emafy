e47e607a0604443f9a7f746187459146fdb3d716
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-18 13:37:43 -0300

    [i18n]: disable rule only for specific lines

--- src/strings.ts
+++ src/strings.ts
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-
 export const strings = {
   components: {
     closeButton: 'Close Button',
@@ -7,12 +5,14 @@ export const strings = {
   scenes: {
     login: {
       signin: 'Sign in',
+      // eslint-disable-next-line prettier/prettier
       instructions: 'To start using the platform, please login using the Spotify API through the link below',
       loginUsingSpotify: 'Login using Spotify',
     },
   },
   api: {
     credentials: {
+      // eslint-disable-next-line prettier/prettier
       errorAuth: 'There was an error during the authentication, please, refresh the webpage and try again',
       errorLoginFailed: 'Login failed',
     },
