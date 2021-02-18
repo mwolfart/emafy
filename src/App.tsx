ea962c5df74bb7af7fe86b62282fd9455d8d2948
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-18 09:47:36 -0300

    [styling]: use ThemeProvider for theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { isLoggedIn, login } from 'api/credentials'
 import React from 'react'
 import './App.css'
-import './themes.sass'
+import { mainTheme } from './themes'
 import { LoginScene } from './scenes/login/login'
+import { ThemeProvider } from 'styled-components'
 
 const App = (): JSX.Element => {
   const isUserLoggedIn = isLoggedIn() || login(() => {})
-  return isUserLoggedIn ? <div>You are logged in</div> : <LoginScene />
+  return (
+    <ThemeProvider theme={mainTheme}>
+      {isUserLoggedIn ? <div>You are logged in</div> : <LoginScene />}{' '}
+    </ThemeProvider>
+  )
 }
 
 export default App
