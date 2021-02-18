729022bd5e5e22cc98bf14689e3f82a2814966f6
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-18 09:47:36 -0300

    [styling]: use ThemeProvider for theming

---
+++ src/themes.ts
@@ -0,0 +1,13 @@
+export const mainTheme = {
+  colorPrimary: '#bf0a45',
+  colorButtonIntermed: '#e4305d',
+  colorSecondary: '#ee7321',
+  colorTextParagraph: '#757575',
+  colorTextTitle: '#222222',
+  colorTextButton: '#eeeeee',
+  colorBackground: '#f2f2f2',
+  colorBackgroundDisabled: '#aaaaaa55',
+  fontStyle: "Montserrat, 'Trebuchet MS', Arial, sans-serif",
+  fontSizeParagraph: '16px',
+  fontSizeTitle: '24px',
+}
