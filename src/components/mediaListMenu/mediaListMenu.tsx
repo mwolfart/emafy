30665d0a742872b17482e8d2c9c11bbdbc5003e2
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-22 10:50:36 -0300

    [media-list]: implement list view of media

---
+++ src/components/mediaListMenu/mediaListMenu.tsx
@@ -0,0 +1,24 @@
+import { MediaRowLink } from 'components/mediaRowLink/mediaRowLink'
+import React from 'react'
+import { VFC } from 'react'
+import styled from 'styled-components'
+import { Media } from 'types/media'
+
+type Props = {
+  mediaList: Media[]
+}
+
+const MediaList = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const MediaListMenu: VFC<Props> = ({ mediaList }) => {
+  return (
+    <MediaList>
+      {mediaList.map((media: Media) => (
+        <MediaRowLink key={media.id} mediaInfo={media} />
+      ))}
+    </MediaList>
+  )
+}
