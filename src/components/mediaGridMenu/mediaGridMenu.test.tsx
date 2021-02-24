dcf30cba93ce4996ddb50ec2e318d7e75de97db7
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-24 16:31:01 -0300

    [media-grid]: use faker for fixtures

--- src/components/mediaGridMenu/mediaGridMenu.test.tsx
+++ src/components/mediaGridMenu/mediaGridMenu.test.tsx
@@ -1,57 +1,19 @@
 import { render } from '@testing-library/react'
 
 import { MediaGridMenu } from './mediaGridMenu'
+import { albums } from 'fixtures/albums'
 import { Album } from 'types/media'
 
 describe('MediaGridMenu', () => {
   it('renders MediaGridMenu correctly', () => {
-    const albumA: Album = {
-      id: '01',
-      name: 'Oceans',
-      artists: [
-        { id: '02', name: 'Foo' },
-        { id: '03', name: 'Bar' },
-      ],
-      totalTracks: 10,
-    }
+    const albumList = albums
 
-    const albumB: Album = {
-      id: '04',
-      name: 'Volcanos',
-      artists: [
-        { id: '02', name: 'Foo' },
-        { id: '03', name: 'Bar' },
-      ],
-      totalTracks: 10,
-    }
+    const { getByText } = render(<MediaGridMenu mediaList={albumList} />)
 
-    const albumC: Album = {
-      id: '05',
-      name: 'Earthquake',
-      artists: [{ id: '02', name: 'Foo' }],
-      totalTracks: 10,
-    }
-
-    const albumD: Album = {
-      id: '06',
-      name: 'Carrot',
-      artists: [{ id: '07', name: 'Gee' }],
-      totalTracks: 10,
-    }
-
-    const albumList = [albumA, albumB, albumC, albumD]
-
-    const { getByText, getAllByText } = render(
-      <MediaGridMenu mediaList={albumList} />,
-    )
-
-    const arr = ['Oceans', 'Foo', 'Volcanos', 'Earthquake', 'Carrot']
-    arr.forEach((query: string): void => {
+    const albumNames = albumList.map((album: Album) => album.name)
+    albumNames.forEach((query: string): void => {
       const element = getByText(query)
       expect(element).toBeTruthy()
     })
-
-    const gridElementList = getAllByText('Foo, Bar')
-    expect(gridElementList.length).toEqual(2)
   })
 })
