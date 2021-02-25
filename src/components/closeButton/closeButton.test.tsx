352c51cb2f3b24635d4dd0ad61928252588f43a7
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-25 09:40:23 -0300

    [media-list]: fix close-button tests

--- src/components/closeButton/closeButton.test.tsx
+++ src/components/closeButton/closeButton.test.tsx
@@ -1,18 +1,23 @@
 import { fireEvent, render } from '@testing-library/react'
+import { strings } from 'strings'
 
 import { CloseButton } from './closeButton'
 
 describe('CloseButton', () => {
   it('renders CloseButton correctly', () => {
     const { getByRole } = render(<CloseButton onClickCallback={() => {}} />)
-    const buttonElement = getByRole('button', { name: 'Close modal' })
+    const buttonElement = getByRole('button', {
+      name: strings.components.closeButton,
+    })
     expect(buttonElement).toBeTruthy()
   })
 
   it('calls onClickCallback on button click', () => {
     const onClick = jest.fn()
     const { getByRole } = render(<CloseButton onClickCallback={onClick} />)
-    const buttonElement = getByRole('button', { name: 'Close modal' })
+    const buttonElement = getByRole('button', {
+      name: strings.components.closeButton,
+    })
     fireEvent.click(buttonElement)
     expect(onClick).toHaveBeenCalled()
   })
