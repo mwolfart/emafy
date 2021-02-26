7e57c1444588fc33993038947752ab3a77302b12
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-26 16:39:53 -0300

    [media-menu]: fix mediaLink test

--- src/components/mediaLink/mediaLink.test.tsx
+++ src/components/mediaLink/mediaLink.test.tsx
@@ -6,9 +6,9 @@ import { album } from 'fixtures/album'
 describe('MediaLink', () => {
   it('renders MediaLink correctly', () => {
     render(<MediaLink mediaInfo={album} />)
-    let linkElement = screen.getByText('Oceans')
+    let linkElement = screen.getByText(album.name)
     expect(linkElement).toBeTruthy()
-    linkElement = screen.getByText('Foo, Bar')
+    linkElement = screen.getByText(`${album.artists[0]}, ${album.artists[1]}`)
     expect(linkElement).toBeTruthy()
   })
 })
