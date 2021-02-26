c5c58462077fb4771e1d726a0d75714b20bd74be
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-26 10:13:54 -0300

    [media-menu]: support list view of menu

--- src/components/mediaMenu/mediaMenu.tsx
+++ src/components/mediaMenu/mediaMenu.tsx
@@ -1,21 +1,33 @@
 import { MediaLink } from 'components/mediaLink/mediaLink'
+import { GlobalProps } from 'globalProps'
 import { VFC } from 'react'
 import styled from 'styled-components'
 import { Media } from 'types/media'
 
 type Props = {
   mediaList: Media[]
+  rowVariant?: boolean
 }
 
-const MediaMenuBlock = styled.div`
+type MenuBlockProps = {
+  rowVariant?: boolean
+} & GlobalProps
+
+const MediaMenuBlock = styled.div<MenuBlockProps>`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(max(240px, 100%/10), 1fr));
+  grid-template-columns: ${(props: MenuBlockProps) =>
+    props.rowVariant
+      ? '1fr'
+      : 'repeat(auto-fill, minmax(max(240px, 100%/10), 1fr))'};
 `
 
-export const MediaMenu: VFC<Props> = ({ mediaList }) => (
-  <MediaMenuBlock>
+export const MediaMenu: VFC<Props> = ({
+  mediaList,
+  rowVariant: isRowVariant,
+}) => (
+  <MediaMenuBlock rowVariant={isRowVariant}>
     {mediaList.map((media: Media) => (
-      <MediaLink key={media.id} mediaInfo={media} />
+      <MediaLink key={media.id} mediaInfo={media} rowVariant={isRowVariant} />
     ))}
   </MediaMenuBlock>
 )
