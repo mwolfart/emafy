a02c4c88895b458f3e82cab60f64fa7b083a41e0
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-02-26 15:38:54 -0300

    [media-menu]: move test data into fixture

--- src/components/mediaLink/mediaLink.test.tsx
+++ src/components/mediaLink/mediaLink.test.tsx
@@ -1,19 +1,10 @@
 import { render } from '@testing-library/react'
 
 import { MediaLink } from './mediaLink'
-import { Album } from 'types/media'
+import { album } from 'fixtures/album'
 
 describe('MediaLink', () => {
   it('renders MediaLink correctly', () => {
-    const album: Album = {
-      id: '01',
-      name: 'Oceans',
-      artists: [
-        { id: '02', name: 'Foo' },
-        { id: '03', name: 'Bar' },
-      ],
-      totalTracks: 10,
-    }
     const { getByText } = render(<MediaLink mediaInfo={album} />)
     let linkElement = getByText('Oceans')
     expect(linkElement).toBeTruthy()
