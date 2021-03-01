dd3ddc2974ed0b554e2fce29dc97d27334c42180
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-03-01 17:51:31 -0300

    [type-guards]: use type guards to interpret types

--- src/types/media.ts
+++ src/types/media.ts
@@ -2,6 +2,7 @@ export type Media = {
   name: string
   images?: Array<string>
   id: string
+  type: 'album' | 'song' | 'artist'
 }
 
 type Reference = string
@@ -9,16 +10,19 @@ type Reference = string
 export interface Album extends Media {
   artists: Array<Media>
   totalTracks: number
+  type: 'album'
 }
 
 export interface Song extends Media {
   artists: Array<Media>
   albumReference: Reference
   duration: number
+  type: 'song'
 }
 
 export interface SimpleArtist extends Media {
   genres: Array<string>
   followers: number
   popularity: number
+  type: 'artist'
 }
