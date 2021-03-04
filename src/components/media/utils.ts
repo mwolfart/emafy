cffdf57306f0a0cb880297683b015b3d0bbec753
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-03-04 16:38:07 -0300

    [components]: refactor reduce & map functions into separate ones

--- src/components/media/utils.ts
+++ src/components/media/utils.ts
@@ -2,14 +2,18 @@ import { isAlbum, isArtist, isSong, Media } from 'types/media'
 
 export const renderSubTitle = (mediaInfo: Media): string => {
   if (isAlbum(mediaInfo) || isSong(mediaInfo)) {
-    return mediaInfo.artists
-      .map((artist: Media) => artist.name)
-      .reduce((accum: string, name: string) => `${accum}, ${name}`)
+    return artistListToString(mediaInfo.artists)
   }
   if (isArtist(mediaInfo)) {
-    return mediaInfo.genres.reduce(
-      (accum: string, genre: string) => `${accum}, ${genre}`,
-    )
+    return genreListToString(mediaInfo.genres)
   }
   return ''
 }
+
+const artistListToString = (artistList: Media[]): string =>
+  artistList
+    .map((artist: Media) => artist.name)
+    .reduce((accum: string, name: string) => `${accum}, ${name}`)
+
+const genreListToString = (genreList: string[]): string =>
+  genreList.reduce((accum: string, genre: string) => `${accum}, ${genre}`)
