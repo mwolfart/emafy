fe57eda3be1d4c7a9d7466fcd3982e071613fa54
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-03-12 10:32:15 -0300

    [saved-albums]: rework authentication in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import { isLoggedIn, login } from 'api/credentials'
-
 import './App.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import '@fortawesome/fontawesome-free/css/solid.min.css'
 import { LoginScene } from 'scenes/login/login'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { mainStyles } from 'styles'
+import { requestValidToken, hasValidToken, hasAuthCode } from 'api/credentials'
 import { SavedAlbums } from 'scenes/savedAlbums/savedAlbums'
+import { useState } from 'react'
 
 const App = (): JSX.Element => {
-  const isUserLoggedIn = isLoggedIn() || login(() => {})
   const GlobalLinkStyle = createGlobalStyle`
     a {
       text-decoration: unset;
     }
   `
 
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(hasValidToken())
+  const [isLoading, setIsLoading] = useState<boolean>(
+    !isLoggedIn && hasAuthCode(),
+  )
+
+  if (isLoading) {
+    requestValidToken(
+      () => {
+        setIsLoggedIn(true)
+        setIsLoading(false)
+      },
+      () => {
+        setIsLoggedIn(false)
+        setIsLoading(false)
+      },
+    )
+  }
+
   return (
     <ThemeProvider theme={mainStyles}>
       <GlobalLinkStyle />
-      {isUserLoggedIn ? <SavedAlbums /> : <LoginScene />}
+      {!isLoading && (isLoggedIn ? <SavedAlbums /> : <LoginScene />)}
     </ThemeProvider>
   )
 }
