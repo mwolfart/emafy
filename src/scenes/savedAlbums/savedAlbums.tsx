21e12a75b004fc7df6688c921fef9c33403ef367
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-03-12 16:55:33 -0300

    [saved-albums]: convert child css selectors to styled components; remove comments

--- src/scenes/savedAlbums/savedAlbums.tsx
+++ src/scenes/savedAlbums/savedAlbums.tsx
@@ -2,56 +2,62 @@ import React, { useEffect, useState, VFC } from 'react'
 import { ToggleDescriptor } from 'components/ui/index'
 import styled from 'styled-components'
 import { Menu as MediaMenu } from 'components/media/menu/menu'
-import { GlobalProps as StyledProps } from 'types/props'
+import { GlobalProps, GlobalProps as StyledProps } from 'types/props'
 import { strings } from 'strings'
 import { getSavedAlbums, NextURL } from 'api/data'
 import { Album } from 'types/media'
 import { mainStyles } from 'styles'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
-// TODO change this
-const Wrapper = styled.div<StyledProps>`
-  ${({ theme = mainStyles }: StyledProps) => `
-    display: flex;
-    flex-direction: column;
+type MenuWrapperProps = {
+  isTransitioning?: boolean
+} & GlobalProps
 
-    .header {
+const Wrapper = styled.div<StyledProps>`
+  display: flex;
+  flex-direction: column;
+`
+
+const Header = styled.div<StyledProps>`
+  ${({ theme = mainStyles }: StyledProps) => `
       display: flex;
       flex-direction: row;
       height: 20%;
       padding: 20px 30px;
       font-family: ${theme.fontStyle};
+  `}
+`
 
-      .description {
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
+const Description = styled.div<StyledProps>`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
 
-        .title {
-          color: ${theme.palette.colorTextTitle};
-          font-size: ${theme.fontSizeTitle};
-        }
+const Title = styled.h1<StyledProps>`
+  ${({ theme = mainStyles }: StyledProps) => `
+    color: ${theme.palette.colorTextTitle};
+    font-size: ${theme.fontSizeTitle};
+    margin: 0;
+  `}
+`
 
-        .subtitle {
-          color: ${theme.palette.colorTextParagraph};
-          font-size: ${theme.fontSizeParagraph};
-        }
-      }
-    }
+const Subtitle = styled.h2<StyledProps>`
+  ${({ theme = mainStyles }: StyledProps) => `
+    color: ${theme.palette.colorTextParagraph};
+    font-size: ${theme.fontSizeParagraph};
+    margin: 0;
+  `}
+`
 
-    .media-menu {
+const MenuWrapper = styled.div<MenuWrapperProps>`
+  ${({ isTransitioning, theme = mainStyles }: MenuWrapperProps) => `
       padding-left: 20px;
       padding-right: 20px;
-      opacity: 1;
-      transition: ${theme.transitionQuickDelayed};
-    }
-
-    .media-menu-transition {
-      padding-left: 20px;
-      padding-right: 20px;
-      opacity: 0;
-      transition: ${theme.transitionQuick};
-    }
+      opacity: ${isTransitioning ? '0' : '1'};
+      transition: ${
+        isTransitioning ? theme.transitionQuick : theme.transitionQuickDelayed
+      };
   `}
 `
 
@@ -71,18 +77,6 @@ export const SavedAlbums: VFC = () => {
       setTransitioning(false)
     }, 250)
   }
-
-  // const scrollCheck = (event: React.UIEvent<HTMLDivElement>): void => {
-  //   const bottom =
-  //     event.currentTarget.scrollHeight - event.currentTarget.scrollTop ===
-  //     event.currentTarget.clientHeight
-  //   if (bottom && nextURL) {
-  //     getSavedAlbums(nextURL).then(({ entities: albumList, next }) => {
-  //       setAlbums(albums.concat(albumList))
-  //       setNextURL(next)
-  //     })
-  //   }
-  // }
 
   const fetchMoreAlbums = (): void => {
     getSavedAlbums(nextURL).then(({ entities: albumList, next }) => {
@@ -107,29 +101,27 @@ export const SavedAlbums: VFC = () => {
         hasMore={albums.length < totalCount}
         loader={''}
       >
-        <div className="header">
-          <div className="description">
-            <div className="title">{strings.scenes.albums.mySavedAlbums}</div>
-            <div className="subtitle">
+        <Header>
+          <Description>
+            <Title>{strings.scenes.albums.mySavedAlbums}</Title>
+            <Subtitle>
               {`${totalCount} ${
                 totalCount > 1
                   ? strings.scenes.albums.subtextAlbums
                   : strings.scenes.albums.subtextAlbum
               }`}
-            </div>
-          </div>
+            </Subtitle>
+          </Description>
           <ToggleDescriptor
             toggleState={displayListView}
             onChangeCallback={changeView}
             labelFalse={strings.scenes.albums.grid}
             labelTrue={strings.scenes.albums.list}
           />
-        </div>
-        <div
-          className={isTransitioning ? 'media-menu-transition' : 'media-menu'}
-        >
+        </Header>
+        <MenuWrapper isTransitioning={isTransitioning}>
           <MediaMenu mediaList={albums} rowVariant={displayListView} />
-        </div>
+        </MenuWrapper>
       </InfiniteScroll>
     </Wrapper>
   )
