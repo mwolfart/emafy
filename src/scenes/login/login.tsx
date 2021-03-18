3f99b95667bdc9fa8dce7e3ff935f5fbbee76fc5
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-03-18 14:16:21 -0300

    [react-router]: use history and move actions into login scene

--- src/scenes/login/login.tsx
+++ src/scenes/login/login.tsx
@@ -1,13 +1,48 @@
-import { VFC } from 'react'
+import { useEffect, useState, VFC } from 'react'
 import { Modal } from 'components/modal/modal'
 import { Button, Canvas } from 'components/ui'
-import { authenticate } from 'api/credentials'
+import {
+  authenticate,
+  hasAuthCode,
+  hasToken,
+  hasValidToken,
+  requestValidToken,
+} from 'api/credentials'
 import { strings } from 'strings'
+import { useHistory } from 'react-router'
 
 export const LoginScene: VFC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(hasValidToken())
+  const [isOnLoginProcess, setIsOnLoginProcess] = useState<boolean>(
+    !isLoggedIn && (hasAuthCode() || hasToken()),
+  )
+
   const apiAuth = (): void => {
     authenticate()
   }
+
+  const onSuccessCallback = (): void => {
+    setIsLoggedIn(true)
+    setIsOnLoginProcess(false)
+  }
+
+  const onErrorCallback = (): void => {
+    setIsLoggedIn(false)
+    setIsOnLoginProcess(false)
+  }
+
+  useEffect(() => {
+    let cancelled = false
+    if (isOnLoginProcess && !cancelled) {
+      requestValidToken({ onSuccessCallback, onErrorCallback })
+    }
+    return () => {
+      cancelled = true
+    }
+  }, [isOnLoginProcess])
+
+  const history = useHistory()
+  !isOnLoginProcess && isLoggedIn && history.push('/saved-albums')
 
   const closeLoginModal = (): void => {}
 
