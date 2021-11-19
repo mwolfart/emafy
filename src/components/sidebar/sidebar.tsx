3c31693da690187aaa08c9c862f83083d95432a4
Murilo Wolfart <murilowolfart@hotmail.com>
2021-11-19 15:48:53 -0300

    [infinite-scroll]: add keys to sidebar buttons and make it fixed

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -9,6 +9,7 @@ type ButtonProps = {
   title: string
   path: string
   icon: string
+  key: number
 }
 
 const Wrapper = styled.div`
@@ -17,6 +18,7 @@ const Wrapper = styled.div`
     flex-direction: column;
     background-color: ${theme.palette.colorBackgroundSidebar};
     height: 100%;
+    position: fixed;
  `}
 `
 
@@ -29,28 +31,47 @@ const mediaButtons: ButtonProps[] = [
     title: strings.components.sidebar.songs,
     path: '/saved-songs/',
     icon: 'fa-music',
+    key: 0,
   },
   {
     title: strings.components.sidebar.albums,
     path: '/saved-albums/',
     icon: 'fa-compact-disc',
+    key: 1,
   },
   {
     title: strings.components.sidebar.artists,
     path: '/saved-artists/',
     icon: 'fa-guitar',
+    key: 2,
   },
   {
     title: strings.components.sidebar.playlists,
     path: '/saved-playlists/',
     icon: 'fa-list',
+    key: 3,
   },
-  { title: strings.components.sidebar.genres, path: '/', icon: 'fa-th' },
+  {
+    title: strings.components.sidebar.genres,
+    path: '/',
+    icon: 'fa-th',
+    key: 4,
+  },
 ]
 
 const miscButtons: ButtonProps[] = [
-  { title: strings.components.sidebar.discover, path: '/', icon: 'fa-search' },
-  { title: strings.components.sidebar.settings, path: '/', icon: 'fa-cog' },
+  {
+    title: strings.components.sidebar.discover,
+    path: '/',
+    icon: 'fa-search',
+    key: 5,
+  },
+  {
+    title: strings.components.sidebar.settings,
+    path: '/',
+    icon: 'fa-cog',
+    key: 6,
+  },
 ]
 
 export const Sidebar: VFC = () => {
@@ -58,8 +79,9 @@ export const Sidebar: VFC = () => {
     title,
     path,
     icon,
+    key,
   }: ButtonProps): JSX.Element => (
-    <SidebarButton title={title} path={path} icon={icon} />
+    <SidebarButton title={title} path={path} icon={icon} key={key} />
   )
   return (
     <Wrapper>
