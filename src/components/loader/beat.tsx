d9aeea6489602af30e54163ee3e618a09d1a6951
Murilo Wolfart <murilowolfart@hotmail.com>
2021-12-06 08:27:56 -0300

    [loader]: add margin to beat loader

--- src/components/loader/beat.tsx
+++ src/components/loader/beat.tsx
@@ -2,12 +2,17 @@ import { mainPalette } from 'palette'
 import { VFC } from 'react'
 import { BeatLoader } from 'react-spinners'
 import styled from 'styled-components'
+import { mainStyles } from 'styles'
+import { GlobalProps } from 'types/global'
 
 const Wrapper = styled.div`
-  display: flex;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
+  ${({ theme = mainStyles }: GlobalProps) => `
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    margin: ${theme.divSpacingBig};
+  `}
 `
 
 export const CustomBeatLoader: VFC = () => {
