dbe736be0f2d739704ccef95e80159d93b8281f0
Murilo Wolfart <murilowolfart@hotmail.com>
2021-12-17 11:06:22 -0300

    [view-album]: move album snippet container to albums folder

--- src/components/media/menu/item/item.tsx
+++ src/components/media/menu/item/item.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { isAlbum, Media } from 'types/media'
 import { MediaExtraProps } from 'types/mediaExtraProps'
 import { AlbumCard } from 'snippets/album/card/card'
-import { SnippetContainer } from '../snippetContainer/snippetContainer'
+import { SnippetContainer as AlbumSnippetContainer } from '../../album/snippetContainer/snippetContainer'
 import { Link as MediaLink } from '../link/link'
 
 type Props = {
@@ -28,12 +28,12 @@ export const Item: VFC<Props> = ({ mediaInfo, rowVariant, extraProps }) => {
         extraProps={extraProps}
       />
       {shouldRenderAlbumSnippet && (
-        <SnippetContainer rowVariant={rowVariant}>
+        <AlbumSnippetContainer rowVariant={rowVariant}>
           <AlbumCard
             albumInfo={mediaInfo}
             fnCloseSnippet={extraProps?.mediaSnippetCloseCallback}
           />
-        </SnippetContainer>
+        </AlbumSnippetContainer>
       )}
     </Wrapper>
   )
