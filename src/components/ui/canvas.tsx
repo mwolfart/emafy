d82819db824b8dbe0d0652a3401f5c31044de574
Murilo Wolfart <murilowolfart@hotmail.com>
2021-12-22 13:44:26 -0300

    [modal]: add responsiveness & make close button optional

--- src/components/ui/canvas.tsx
+++ src/components/ui/canvas.tsx
@@ -4,6 +4,7 @@ export const Canvas = styled.div`
   ${({ theme }) => `
     position: absolute;
     min-height: 100%;
+    height: 100%;
     min-width: 100vw;
     display: flex;
     justify-content: center;
